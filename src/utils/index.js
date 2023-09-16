17de67c476a0b31a9f645a5c3ca0bc556f973c81
Alexander Petrov <sverdans@192.168.0.6>
2023-09-16 22:18:44 +0300

    date formats fixed

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,77 +1,76 @@
-
-const dayNames = [ 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс' ]
-const monthNames = [ 
-    'January', 'February', 'March',
-    'April',   'May',      'June',
-    'July',    'August',   'September',
-    'October', 'November', 'December',
-]
+import moment from 'moment'
 
 export const getDate = (isoDate) => 
 {
-    const date = new Date(isoDate);
+    const now = moment()
+    const date = moment(isoDate, true)
 
-    const diffDays = parseInt((new Date() - date) / (1000 * 60 * 60 * 24), 10); 
+    const diffDays = date.diff(now, 'days');
 
     if (diffDays < 1)
-        return date.getHours() + ':' + date.getMinutes()
+    {
+        return date.format("HH:MM")
+    }
 
     if (diffDays < 7)
-        return dayNames[date.getDay()]
+    {
+        return date.format("dd")
+    }
 
-    return date.toISOString().split('T')[0].replace('-', '.')
+    return date.format("DD.MM.YYYY")
 }
 
 export const getMessageTime = (isoDate) => 
 {
-    const date = new Date(isoDate);
-    return date.getHours() + ':' + date.getMinutes()
+    return moment(isoDate, true).format("HH:MM")
 }
 
 export const getChatDate = (isoDate) =>
 {
-    const date = new Date(isoDate);
-    const now = new Date();
+    const date = moment(isoDate, true)
+    const diffYears = date.diff(moment(), 'years')
 
-    if (date.getFullYear() !== now.getFullYear())
-        return monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()
+    if (diffYears > 0)
+    {
+        return date.format("MMMM D, YYYY")
+    }
 
-    return monthNames[date.getMonth()] + ' ' + date.getDate()
+    return date.format("MMMM D")
 }
 
 export const stringToColor = (string) => 
 {
-	let hash = 0;
-	let i;
-  
-	for (i = 0; i < string.length; i += 1) 
-	  	hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  
-	let color = '#';
-  
-	for (i = 0; i < 3; i += 1) 
-	{
-		const value = (hash >> (i * 8)) & 0xff;
-		color += `00${value.toString(16)}`.slice(-2);
-	}
-  
-	return color;
+    let hash = 0;
+    let i;
+
+    for (i = 0; i < string.length; i += 1) 
+        hash = string.charCodeAt(i) + ((hash << 5) - hash);
+
+    let color = '#';
+
+    for (i = 0; i < 3; i += 1) 
+    {
+        const value = (hash >> (i * 8)) & 0xff;
+        color += `00${value.toString(16)}`.slice(-2);
+    }
+
+    return color;
 }
-  
+
 export const stringAvatar = (name, size) => 
 {
-	const words = name.split(' ')
-	size = size + 'px'
+    const words = name.split(' ')
+    size = size + 'px'
 
-	let text;
-	if (words[1] !== '')
-		text = `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`
-	else
-		text = `${name.split(' ')[0][0]}`
+    let text;
+    if (words[1] !== '')
+        text = `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`
+    else
+        text = `${name.split(' ')[0][0]}`
 
-	return {
-		sx: { bgcolor: stringToColor(name), color: 'text.primary', fontWeight: 'bold',
-		 	  width: size, height: size},
-		children: text,
-	};
+    return {
+        sx: { bgcolor: stringToColor(name), color: 'text.primary', fontWeight: 'bold',
+            width: size, height: size},
+        children: text,
+    };
 }
